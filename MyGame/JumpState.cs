ddcae8f860239989b6d4726e0ba30d6e62115ae5
Jossean Rivera <jossean.yamil@hotmail.com>
2017-11-04 19:17:45 -0400

    Fixing handle input of JumpState to set the state to Idle when user is not pressing Left or Right.

--- MyGame/JumpState.cs
+++ MyGame/JumpState.cs
@@ -9,16 +9,21 @@ namespace MyGame
         private Hero _hero; //We need the hero instance to be able to be at the draw method
         private const int vely = 10; //VelocityY = 10 pixels / 1 Frame
         private const double velx = 0.3; //VelocityX = 0.3 pixels / 1ms
-        private int _frametotal;
+
         public override HeroState HandleInput(Hero h)
         {
+            if (_framecount >= _frametotal)
+                if (Keyboard.IsKeyDown(Key.Right))
+                    return new RunState(h, HeroDirection.Right);
+                else if (Keyboard.IsKeyDown(Key.Left))
+                    return new RunState(h, HeroDirection.Left);
+                else
+                    return new IdleState(h, Direction);
+
             if (Keyboard.IsKeyDown(Key.Right))
                 _input = Key.Right;
             else if (Keyboard.IsKeyDown(Key.Left))
                 _input = Key.Left;
-
-            else if (Keyboard.IsKeyUp(Key.Right) & Keyboard.IsKeyUp(Key.Down))
-                _input = Key.Z;
 
             return null;
         }
@@ -39,34 +44,32 @@ namespace MyGame
             }
             else if (_framecount <= _frametotal)
             {
+                //Go down
                 hero.Position.Y += vely;
-                if (_framecount == _frametotal & _delaycounter == ImgDelay)
-                {
-                    _hero = hero;
-                    //hero.Position.Y -= vely;
-                }
+                //if (_framecount == _frametotal & _delaycounter == ImgDelay)
+                //    _hero = hero;
             }
         }
 
         public override void Draw(Graphics g, int x, int y)
         {
             base.Draw(g, x, y);
-            if(_hero != null)
-            {
-                _hero.State = new RunState(_hero, this.Direction);
-                _hero.State.Enter(_hero);
-            }
+            //if(_hero != null)
+            //{
+            //    _hero.State = new RunState(_hero, this.Direction);
+            //    _hero.State.Enter(_hero);
+            //}
         }
 
         public override void Enter(Hero hero)
         {
             base.Enter(hero);
-            _hero = null;
+            //_hero = null;
         }
 
-        public JumpState(Hero h, HeroDirection d) : base("Jump", h.Width, h.Height, d)
+        public JumpState(Hero h, HeroDirection d) : base("Jump", h.Width + GameWorld.TILES_WIDTH * 1/4, h.Height, d)
         {
-            _frametotal = (ImgDelay + 1) * ImgsCount;
+            
         }
     }
 }
